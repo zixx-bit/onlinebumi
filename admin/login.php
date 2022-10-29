bad301fe9c2a51f9a18303cd99a34747e54dbc42
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-10-30 00:04:20 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,6 @@
 <?php
 // error_reporting(1);
- require_once $_SERVER['DOCUMENT_ROOT']. '/online store/core/init.php';
+ require_once $_SERVER['DOCUMENT_ROOT']. '/core/init.php';
  // include 'includes/head.php';
  $name ="";
  $email ="";
