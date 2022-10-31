f13631c5f6ad90faaefa8be49c0a5207c044bc7e
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-01 00:49:07 +0300

    Update verify_email.php

--- verify/verify_email.php
+++ verify/verify_email.php
@@ -21,7 +21,7 @@ if (isset($_GET['token'])) {
             $_SESSION['verified'] = true;
             $_SESSION['message'] = "Your email address has been verified successfully";
             $_SESSION['type'] = 'alert-success';
-            // header('location: index.php');
+            header('location: index.php');
             exit(0);
         }
     } else {
