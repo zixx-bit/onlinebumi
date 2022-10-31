7ef7d78d37bf5e7834d9c43c223222d6ca86db4f
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-01 00:35:43 +0300

    Update verify_email.php

--- verify/verify_email.php
+++ verify/verify_email.php
@@ -1,16 +1,18 @@
 <?php
 session_start();
 
-$conn = new mysqli('localhost', 'root', '', 'verify-user');
+$conn = new mysqli('localhost', 'bitray', '802neoKenya', 'store');
 
 if (isset($_GET['token'])) {
     $token = $_GET['token'];
     $sql = "SELECT * FROM verify WHERE token='$token' LIMIT 1";
-    $result = mysqli_query($conn, $sql);
+
+      $result = mysqli_query($conn, $sql);
+      // var_dump($result);
 
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
-        $query = "UPDATE users SET verified=1 WHERE token='$token'";
+        $query = "UPDATE verify SET verified = 1 WHERE token='$token'";
 
         if (mysqli_query($conn, $query)) {
             $_SESSION['id'] = $user['id'];
