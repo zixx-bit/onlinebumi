b9f2689295f5dcb4f2d266fb3a3f27d2c2fff11d
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-02 00:44:48 +0300

    Update authController.php

--- verify/controllers/authController.php
+++ verify/controllers/authController.php
@@ -74,7 +74,7 @@ if (isset($_POST['login-btn'])) {
     $password = $_POST['password'];
 
     if (count($errors) === 0) {
-        $query = "SELECT * FROM verify WHERE username=? OR email=? LIMIT 1";
+        $query = "SELECT * FROM users WHERE full_name=? OR email=? LIMIT 1";
         $stmt = $conn->prepare($query);
         $stmt->bind_param('ss', $username, $email);
 
