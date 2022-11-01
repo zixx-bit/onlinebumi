07ee2a3b8c22541366a897c033076a71dc724c9c
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-02 00:27:33 +0300

    Update index.php

--- verify/index.php
+++ verify/index.php
@@ -45,7 +45,7 @@ if (empty($_SESSION['id'])) {
             <strong><?php echo $_SESSION['email']; ?></strong>
           </div>
         <?php else: ?>
-          <button href="" class="btn btn-lg btn-primary btn-block">Proceed >></button>
+          <a href="../admin/products.php" class="btn btn-lg btn-primary btn-block">Proceed >></a>
         <?php endif;?>
       </div>
     </div>
