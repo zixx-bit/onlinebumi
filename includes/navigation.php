c2632c7ae2a9ae3b270ca4a60cc722c869c4239a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-07 17:31:27 +0300

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -25,8 +25,8 @@ $pquery = $db->query($sql);
 
             <li><a class="btn-sm button-86" href="admin/products.php?add"
               style=" background-image: linear-gradient(to bottom right, #fcd34d, #ef4444, #ec4899);"> Submit Property+ </a></li> <br>
-            <li><a class="btn-sm button-70" href="admin/"> sign Up </a></li> <br>
-            <li><a class=" button-70" href="admin/login.php"> <span class="glyphicon glyphicon-log-in"> Login</span> </a></li>
+            <li><a class="btn-sm button-70" href="admin/"><span class="glyphicon glyphicon-log-out"></span> sign Up </a></li> <br>
+            <li><a class=" button-70" href="admin/login.php"> <span class="glyphicon glyphicon-log-in"> </span>  Login</a></li>
            	</ul>
 
      </div> <!-- navbar-collapse.// -->
