b1fd8d7992487f505dc84e1866e3a9badf6cad5a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-17 06:25:52 +0300

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']. '/online store/core/init.php';
 unset($_SESSION['id']);
+unset($_SESSION['username']);
+unset($_SESSION['email']);
+unset($_SESSION['verify']);
 header('Location: ../verify/login.php');
-?>
