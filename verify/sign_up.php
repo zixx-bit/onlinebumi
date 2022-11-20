89f7ed63fc1ca0de43a4a0c232231b1379a8746e
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-20 18:01:01 +0300

    Update sign_up.php

--- verify/sign_up.php
+++ verify/sign_up.php
@@ -9,7 +9,9 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" /> -->
   <link rel="stylesheet" href="main.css">
   <title>User verification</title>
 </head>
@@ -81,7 +83,7 @@
 
 
           <!-- permissions -->
-          <div class="form-group" style="display:hidden;">
+          <div class="form-group" style="display:none;">
             <label for="permissions" id="permissions_sign">Permissions:</label>
             <select class="form-control" name="permissions">
               <!-- <option value=""<?=(($permissions == '')?' selected':'' );?>></option> -->
