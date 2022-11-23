a481fd1e2b4f482cba733f789ecc27174cb34196
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2022-11-24 00:01:22 +0300

    Update brands.php

--- admin/brands.php
+++ admin/brands.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once $_SERVER['DOCUMENT_ROOT'].'/online store/core/init.php';
 if (!is_logged_in()) {
